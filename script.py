4091101822c8692bd6ed851f7a7d597d5dbc22e5
Mustakim Ahmed Hasan <mustakimahmedhasan@Mustakims-MacBook-Air.local>
2023-09-14 03:15:52 -0400

    The project now creates folders and sorts according to the file types

--- script.py
+++ script.py
@@ -1,5 +1,7 @@
 import pathlib, os, shutil
 
+print("RUNNING")
+
 source_path = pathlib.Path.home()/'Downloads/'
 
 # New folder directories
@@ -7,12 +9,14 @@ images_path = pathlib.Path.home()/'Downloads/Images'
 docs_path = pathlib.Path.home()/'Downloads/Docs'
 zip_path = pathlib.Path.home()/'Downloads/Zip'
 dmg_path = pathlib.Path.home()/'Downloads/Dmg'
+app_path = pathlib.Path.home()/'Downloads/Apps'
 
 #extensions
 image_extensions = ('jpg', 'jpeg', 'png')
-doc_extensions = ('docx', 'pdf')
+doc_extensions = ('docx', 'pdf', 'pkpass')
 zip_extension = 'zip'
 dmg_extension = 'dmg'
+app_extension = 'app'
 
 def file_mover(destination_path, extensions):
     source_path= pathlib.Path.home()/'Downloads/'
@@ -25,15 +29,17 @@ def file_mover(destination_path, extensions):
 
     for file_name in os.listdir(source_path):
         if file_name.lower() == extensions:
-            break
+            pass
         elif file_name.lower().endswith(extensions):
+            print(file_name)
             source_file_path = os.path.join(source_path, file_name)
             destination_file_path = os.path.join(destination_path, file_name)
 
             shutil.move(source_file_path, destination_file_path)
-            print('file moved!')
+            print('file moved!', extensions)
 
 file_mover(images_path, image_extensions)
 file_mover(docs_path, doc_extensions)
-# file_mover(zip_path, zip_extension)
+file_mover(zip_path, zip_extension)
 file_mover(dmg_path, dmg_extension)
+file_mover(app_path, app_extension)
